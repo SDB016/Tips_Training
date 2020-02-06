8977f21193ecc156cffd6746900902209712f102
shin <dongbin4013@naver.com>
2020-02-06 18:21:39 +0900

    commit test

--- Mywin32_Training_1/Mywin32_Training_1.cpp
+++ Mywin32_Training_1/Mywin32_Training_1.cpp
@@ -4,7 +4,7 @@
 
 #define MAX_LOADSTRING 100
 
-// 전역 변수:
+//   전역 변수:
 HINSTANCE hInst;                                // 현재 인스턴스입니다.
 WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
 WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
